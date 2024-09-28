4719734d9ba83ebca43aaaeff2b45053f66e2004
Qingchen_Jia <879484952@qq.com>
2024-09-28 12:33:11 +0800

    Add isDelete to BookType to realize logical deletion

--- src/main/java/BookManageSystem/mapper/BookTypeMapper.java
+++ src/main/java/BookManageSystem/mapper/BookTypeMapper.java
@@ -7,22 +7,25 @@ import java.util.List;
 
 @Mapper
 public interface BookTypeMapper {
-    @Select("SELECT * FROM book_type")
+    @Select("SELECT * FROM book_type WHERE is_delete=0")
     @ResultMap("BookType")
     List<BookType> selectAll();
 
-    @Select("SELECT * FROM book_type WHERE tid=#{tid}")
+    @Select("SELECT * FROM book_type WHERE tid=#{tid} AND is_delete=0")
     @ResultMap("BookType")
     BookType selectByTid(String tid);
 
-    @Select("SELECT * FROM book_type WHERE type_name=#{typeName}")
+    @Select("SELECT * FROM book_type WHERE type_name=#{typeName} AND is_delete=0")
     @ResultMap("BookType")
     BookType selectByTypeName(String typeName);
 
-    @Insert("INSERT INTO book_type VALUES (#{tid},#{typeName})")
+    @Insert("INSERT INTO book_type VALUES (#{tid},#{typeName},0)")
     @ResultMap("BookType")
     void insertBookType(BookType bookType);
 
-    @Delete("DELETE FROM book_type WHERE tid=#{tid}")
+    @Update("UPDATE book_type SET is_delete=1 WHERE tid=#{tid}")
     void deleteByTid(String tid);
+
+    @Select("SELECT COUNT(*) FROM book_info WHERE tid=#{tid}")
+    int selectBookNumByTid(String Tid);
 }
--- src/main/java/BookManageSystem/pojo/BookType.java
+++ src/main/java/BookManageSystem/pojo/BookType.java
@@ -10,4 +10,5 @@ import lombok.NoArgsConstructor;
 public class BookType {
     private String tid;
     private String typeName;
+    private int isDelete;
 }
