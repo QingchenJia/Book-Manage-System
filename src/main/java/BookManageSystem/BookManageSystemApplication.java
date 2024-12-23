77c6f74c46c2eb923d462dd21353c209d753b0bf
Qingchen_Jia <879484952@qq.com>
2024-12-23 17:24:29 +0800

    Print the website url in console to access it conveniently

--- src/main/java/BookManageSystem/BookManageSystemApplication.java
+++ src/main/java/BookManageSystem/BookManageSystemApplication.java
@@ -1,15 +1,22 @@
 package BookManageSystem;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("BookManageSystem.mapper")
+@Slf4j
 public class BookManageSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookManageSystemApplication.class, args);
+        log.info("""
+                
+                ----------------------------------------------------------
+                \tWeb-Url:\thttps://127.0.0.1:9090/index.html
+                ----------------------------------------------------------""");
     }
 
 }
